1083ad11871f745d61101ac190ffdfe464f75edf
Yasmeen Hesham <yasmeen.hesham305@gmail.com>
2020-04-23 16:07:17 +0200

    Register,profile , logout & switch

--- lib/homemenues.dart
+++ lib/homemenues.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nodustmobileapp/Models/user.dart';
 import 'package:nodustmobileapp/home.dart';
 import 'package:nodustmobileapp/mycards.dart';
+import 'package:nodustmobileapp/myprofile.dart';
 import 'package:nodustmobileapp/viewProducts.dart';
 
 import 'AboutUs.dart';
@@ -40,22 +41,6 @@ class _HomeMenuState extends State<HomeMenu> {
 
   @override
   Widget build(BuildContext context) {
-
-
-    Widget _bottomNavigationBar(int selectedIndex) => BottomNavigationBar(
-      onTap: (int index) => setState(() => _currentIndex = index),
-      currentIndex: selectedIndex,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon:  Icon(Icons.home), title:  Text('Home'),),
-        BottomNavigationBarItem(
-          icon:  Icon(Icons.credit_card), title:  Text('My Cards'),),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart), title: Text('Products'),),
-      ],
-    );
-
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -64,7 +49,7 @@ class _HomeMenuState extends State<HomeMenu> {
           Padding(
               padding: EdgeInsets.only(right: 20.0),
               child: GestureDetector(
-                onTap: () {},
+                onTap: _viewProfile,
                 child: Icon(
                   Icons.account_circle,
                   size: 40.0,
@@ -74,62 +59,68 @@ class _HomeMenuState extends State<HomeMenu> {
       ),
       drawer: new Drawer(
           child: new ListView(
-        children: <Widget>[
-            new DrawerHeader(
-             child: new Text(''),
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                    image: AssetImage("images/logo.jpg"), fit: BoxFit.cover)),
-          ),
+            children: <Widget>[
+              new DrawerHeader(
+                child: new Text(''),
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        image: AssetImage("images/logo.jpg"), fit: BoxFit.cover)),
+              ),
 
-            new ListTile(
-              title: new Text('Home'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-           new ListTile(
-              title: new Text('About Us'),
-            onTap: () {
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (context) => new AboutUs()));
-            },
-          ),
+              new ListTile(
+                leading: Icon(Icons.home),
+                title: new Text('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              new ListTile(
+                leading: Icon(Icons.supervisor_account),
+                title: new Text('About Us'),
+                onTap: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => new AboutUs()));
+                },
+              ),
 
-            new ListTile(
-              title: new Text('Notifications'),
-              onTap: () {
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (context) => new Notifications()));
-            },
-          ),
+              new ListTile(
+                leading: Icon(Icons.notifications),
+                title: new Text('Notifications'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new Notifications()));
+                },
+              ),
 
-          //  new Divider(),
-            new ListTile(
-              title: new Text('Claim your points'),
-              onTap: () {
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (context) => new ClaimYourPoints()));
-            },
-            ),
-            new ListTile(
-              title: new Text('Contact Us'),
-              onTap: () {
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (context) => new ContactUs()));
-            },
-            ),
-            new ListTile(
-              title: new Text('Invite'),
-              onTap: () {},
-          ),
-        ],
-      )),
+              //  new Divider(),
+              new ListTile(
+                leading: Icon(Icons.trending_up),
+                title: new Text('Claim your points'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new ClaimYourPoints()));
+                },
+              ),
+              new ListTile(
+                leading: Icon(Icons.call),
+                title: new Text('Contact Us'),
+                onTap: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => new ContactUs()));
+                },
+              ),
+              new ListTile(
+                leading: Icon(Icons.person_add),
+                title: new Text('Invite'),
+                onTap: () {},
+              ),
+            ],
+          )),
       bottomNavigationBar:BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
@@ -164,10 +155,10 @@ class _HomeMenuState extends State<HomeMenu> {
               icon: Icon(Icons.shopping_cart), title: Text('Products'))
         ],
       ),*/
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _children,
-        ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _children,
+      ),
       /*PageStorage(
           child: _children[_currentIndex],
           bucket: bucket,
@@ -176,18 +167,32 @@ class _HomeMenuState extends State<HomeMenu> {
     );
   }
   void _onItemTapped(int index) {
-   // print(index);
+    // print(index);
     if(index==1&&userLoad ==null){
-    Navigator.of(context)
-        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-    return new Login();
-        }));
-        }
+      Navigator.of(context)
+          .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+        return new Login();
+      }));
+    }
     else{
-        setState(() {
+      setState(() {
         _currentIndex = index;
-        });
-        }
+      });
+    }
+  }
+  _viewProfile()
+  {
+    if(userLoad ==null)
+    {
+      Navigator.of(context)
+          .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+        return new Login();
+      }));
+    }
+    else{
+      Navigator.push(context,
+          new MaterialPageRoute(builder: (context) => new Profile()));
+    }
   }
   loadSharedPrefs() async {
     try {
